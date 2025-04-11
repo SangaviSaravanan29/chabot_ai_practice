fbf268c6c8a0b24f8c7b73b91bee5a5cbd6da637
sangavi <sangavi.infomover@gmail.com>
2025-04-11 17:51:41 +0530

    added Semantic Search  langchain

---
+++ langchain_training/semantic_search_langchain.py
@@ -0,0 +1,43 @@
+import os
+import getpass
+from dotenv import load_dotenv
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Load environment variables from .env file
+load_dotenv()
+
+if __name__ == "__main__":
+    # Get Mistral AI API key
+    api_key = os.getenv("MISTRAL_API_KEY")
+    if not api_key:
+        api_key = getpass.getpass("Enter API key for Mistral AI: ")
+
+    pdf_path = os.getenv("PDF_PATH")
+
+    # Initialize the Mistral AI embedding model
+    embeddings = MistralAIEmbeddings(api_key=api_key, model="mistral-embed")
+
+    # Load documents from a PDF file
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+
+    # Split documents into smaller chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = text_splitter.split_documents(documents)
+
+    # Create an in-memory vector store and add document splits
+    vector_store = InMemoryVectorStore(embeddings)
+    vector_store.add_documents(all_splits)
+
+    # Perform a similarity search
+    query = "What is Math"
+    results = vector_store.similarity_search(query)
+
+    # Print the top result
+    if results:
+        print(results[0])
+    else:
+        print("No results found.")
